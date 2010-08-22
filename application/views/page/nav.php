163f2cc0c3a3c58f7ab1b54b03dbfeb8bfc59818
Richard Willis <willis.rh@gmail.com>
2010-08-22 10:44:09 +0100

    navigation tweaks

--- application/views/page/nav.php
+++ application/views/page/nav.php
@@ -2,28 +2,13 @@
 
 	<ul>	
 	<?php if (Auth::instance()->logged_in()) {?>
-		<li class="ui-helper-hidden">
+		<li>
 			<a id="projects-link" href="<?php echo URL::site('projects') ?>" class="ui-state-default">
-				<span class="ui-icon ui-icon-triangle-1-s helper-right"></span>
 				Projects
 			</a>
-
-			<ul class="ui-helper-hidden-accessible">
-				<?php if (isset($projects)){
-					foreach($projects as $project){?>
-						<li><a href="#"><?php echo $project->name ?></a></li>
-					<?php }
-				} ?>
-				<li id="projects-new">
-					<a href="#">New project</a>
-					<input type="text" name="project-name" />
-				</li>
-			</ul>
-		</li>
-		<li>
+			|
 			<a href="<?php echo URL::site('reports') ?>">Reports</a>
-		</li>
-		<li>
+			|
 			<a href="<?php echo URL::site('sign-out') ?>">Sign out</a>
 		</li>
 	<?php } else {?>
